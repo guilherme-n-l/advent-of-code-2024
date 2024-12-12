21a84591462f61ba78f6a995a3c59f9d6886f6ab
guilherme-n-l <acc.guilhermenl@gmail.com>
2024-12-12 13:48:22 -0300

    (d11) Done fr now

--- d11/d11.c
+++ d11/d11.c
@@ -12,14 +12,27 @@
 #define PILE(A, B, C) (A[B++] = C)
 
 int nums[NUMS_LEN], nums_len = 0;
-GHashTable *ht; 
+GHashTable *blink_cache, *recursive_cache; 
 
 typedef struct {long l1; long l2;} longs;
+
+void get_input() {
+    FILE *f = fopen("input.txt", "r");
+    char ln[LINE_LEN + 2];
+    fgets(ln, sizeof(ln), f);
+    
+    char *tok = strtok(ln, " ");
+
+    while (tok) {
+        nums[nums_len++] = atoi(tok);
+        tok = strtok(NULL, " ");
+    }
+}
 
 longs blink(long n) {
     char num_str[250];
 
-    longs *val_ptr = g_hash_table_lookup(ht, GUINT_TO_POINTER(n));
+    longs *val_ptr = g_hash_table_lookup(blink_cache, GUINT_TO_POINTER(n));
 
     if (val_ptr) return *val_ptr;
 
@@ -27,7 +40,7 @@ longs blink(long n) {
     switch (n) {
         case 0: {
             val->l1 = 1, val->l2 = -1;
-            g_hash_table_insert(ht, GUINT_TO_POINTER(n), val);
+            g_hash_table_insert(blink_cache, GUINT_TO_POINTER(n), val);
             return *val;
         }
         default:
@@ -35,7 +48,7 @@ longs blink(long n) {
 
             if (strlen(num_str) & 1) {
                 val->l1 = 2024 * n, val->l2 = -1;
-                g_hash_table_insert(ht, GUINT_TO_POINTER(n), val);
+                g_hash_table_insert(blink_cache, GUINT_TO_POINTER(n), val);
                 return *val;
             }
             else {
@@ -51,63 +64,92 @@ longs blink(long n) {
                 slice[half_size] = '\0';
                 val->l2 = atol(slice);
 
-                g_hash_table_insert(ht, GUINT_TO_POINTER(n), val);
+                g_hash_table_insert(blink_cache, GUINT_TO_POINTER(n), val);
                 return *val;
             }
     }
 }
 
-long solve(int repeat) {
-    int num_depth_len  = 0;
-    longs num_depth[nums_len + REPEAT_PT2];
-    long collector = 0;
+long recursive(long id, long d, int repeat) {
+    if (d < repeat) {
+        longs blinked = blink(id);
 
-    for (int i = 0; i < nums_len; i++) PILE(num_depth, num_depth_len, ((longs){nums[i], 0}));
-
-    while (num_depth_len > 0) {
-        longs head = POLL(num_depth, num_depth_len);
-
-        if (head.l2 == repeat) {
-            collector++;
-            continue;
-        }
+        longs *left = g_malloc(sizeof(longs));
+        left->l1 = blinked.l1, left->l2 = d + 1;
+        long *left_val_ptr = g_hash_table_lookup(recursive_cache, left);
         
-        longs blinked = blink(head.l1);
-        PILE(num_depth, num_depth_len, ((longs){blinked.l1, head.l2 + 1}));
-        if (blinked.l2 != -1) PILE(num_depth, num_depth_len, ((longs){blinked.l2, head.l2 + 1}));
+        long l_res;
+        if (!left_val_ptr) {
+            l_res = recursive(blinked.l1, d + 1, repeat);
+            left_val_ptr = g_malloc(sizeof(long));
+            *left_val_ptr = l_res;
+            g_hash_table_insert(recursive_cache, left, left_val_ptr);
+        } else {
+            g_free(left);
+            l_res = *left_val_ptr;
+        }
+
+        if (blinked.l2 == -1) return l_res;
+
+        longs *right = g_malloc(sizeof(longs));
+        right->l1 = blinked.l2, right->l2 = d + 1;
+        long *right_val_ptr = g_hash_table_lookup(recursive_cache, right);
+        
+        long r_res;
+        if (!right_val_ptr) {
+            r_res = recursive(blinked.l2, d + 1, repeat);
+            right_val_ptr = g_malloc(sizeof(long));
+            *right_val_ptr = r_res;
+            g_hash_table_insert(recursive_cache, right, right_val_ptr);
+        } else {
+            g_free(right);
+            r_res = *right_val_ptr;
+        }
+
+        return l_res + r_res;
     }
 
-    return collector;
+    return 1;
 }
 
-void print(gpointer k, gpointer v, gpointer args) {
-    long key = GPOINTER_TO_UINT(k);
-    longs *val = (longs *)v;
-
-    printf("%ld -> %ld %ld\n", key, val->l1, val->l2);
+gboolean equal(gconstpointer a, gconstpointer b) {
+    const longs *a_ptr = (longs *)a;
+    const longs *b_ptr = (longs *)b;
+    return a_ptr->l1 == b_ptr->l1 && a_ptr->l2 == b_ptr->l2;
 }
 
-void free_longs(gpointer k, gpointer v, gpointer args) {
+guint hash(gconstpointer a) {
+    const longs *a_ptr = (longs *)a;
+    return g_int_hash(&a_ptr->l1) ^ g_int_hash(&a_ptr->l2);
+}
+
+void free_values(gpointer k, gpointer v, gpointer args) {
     g_free(v);
 }
 
+void free_keys(gpointer k, gpointer v, gpointer args) {
+    g_free(k);
+}
+
+long solve(int repeat) {
+    blink_cache = g_hash_table_new(g_direct_hash, g_direct_equal);
+    recursive_cache = g_hash_table_new(hash, equal);
+
+    long collector = 0;
+    for (int i = 0; i < nums_len; i++) collector += recursive(nums[i], 0, repeat);
+
+    g_hash_table_foreach(blink_cache, free_values, NULL);
+    g_hash_table_destroy(blink_cache);
+    g_hash_table_foreach(recursive_cache, free_keys, NULL);
+    g_hash_table_foreach(recursive_cache, free_values, NULL);
+    g_hash_table_destroy(recursive_cache);
+    return collector;
+}
+
 int main() {
-    FILE *f = fopen("input.txt", "r");
-    char ln[LINE_LEN + 2];
-    fgets(ln, sizeof(ln), f);
-    
-    char *tok = strtok(ln, " ");
-
-    while (tok) {
-        nums[nums_len++] = atoi(tok);
-        tok = strtok(NULL, " ");
-    }
-
-    ht = g_hash_table_new(g_direct_hash, g_direct_equal);
+    get_input();
     printf("%ld\n", solve(REPEAT));
     printf("%ld\n", solve(REPEAT_PT2));
-    g_hash_table_foreach(ht, free_longs, NULL);
-    g_hash_table_destroy(ht);
 
     return 0;
 }
